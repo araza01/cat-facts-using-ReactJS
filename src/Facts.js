66497e32bc12cf9d4072fcdfbec41f85d37b6d09
Amir Raza <62903819+araza01@users.noreply.github.com>
2020-09-03 19:29:21 +0530

    Add files via upload

---
+++ src/Facts.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+export default class CatFacts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: true,
+            error: null,
+            items: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://cat-fact.herokuapp.com/facts/random?amount=5")
+        .then(response => response.json())
+        .then((result) => {
+            this.setState({
+                isLoaded: true,
+                items: result.items
+            });
+        }, (error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        })
+    }
+
+    render() {
+        const isLoaded = this.state.isLoaded;
+        const error = this.state.error;
+        const items = this.state.items;
+        
+        if(error) {
+            return (
+                <section className="container">
+                    <article className="fact-font">
+                        Error: {error.message}
+                    </article>
+                </section>
+            );
+        } else if(!isLoaded) {
+            return (
+                <section className="container">
+                    <article className="fact-font">
+                        Loading<i className="fa fa-spinner fa-spin"></i>
+                    </article>
+                </section>
+            );
+        }
+        return (
+            <section className="container">
+                <article className="heading">
+                    <div>Some interesting facts about CAT:</div>
+                </article>
+                <article className="fact-container fact-font">
+                    <ul>
+                        {
+                            items.map((fact) => {
+                                <li key={fact.id}>{fact.text}</li>
+                            })
+                        }
+                    </ul>
+                </article>
+                <article className="btn">
+                    <button>Get more facts</button>
+                </article>
+            </section>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import List from './List'
+import CatFacts from './Facts'
+
+// const arr = [10, 20, 30, 40]
+ReactDOM.render(<CatFacts  />, document.getElementById("root"));
